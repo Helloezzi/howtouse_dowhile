9373bb0d75f7e899c0aa12ae421b1ca61e1029d3
hwjung <hwjung@zinnotech.com>
2020-02-18 21:10:19 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Linq;
+
+namespace dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("start");
+            int i = 0;
+            do
+            {
+                i++;
+                Console.WriteLine($"{i}");
+            }while(i < 10);
+            Console.WriteLine("end");
+        }
+
+        public static async void ReadAsync(HttpResponseMessage msg)
+        {
+            using (var stream = await msg.Content.ReadAsStreamAsync()) 
+            {
+                var totalRead = 0;
+                var buffer = new byte[1024];
+                var isMoreRead = true;
+                
+                do 
+                {
+                    var read = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (read == 0)
+                    {
+                        isMoreRead = false;
+                    }                        
+                    else
+                    {
+                        var data = new byte[read];
+                        buffer.ToList().CopyTo(0, data, 0, read);
+                        totalRead += read;
+                    }
+                }while(isMoreRead);
+            }
+        }
+    }    
+}
